c1282f790316af98b79319ed44942bbefabf357f
Juan villarroel <vilfrack21@gmail.com>
2021-04-08 21:00:07 -0300

    se crea proyecto usando retrofit, corrutinas y picaso

--- app/src/main/java/com/retrofit/MainActivity.kt
+++ app/src/main/java/com/retrofit/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.retrofit
 
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.retrofit.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import com.retrofit.APIService as APIService1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var adapter:DogAdapter
+    private val dogImages = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.svDogs.setOnQueryTextListener(this)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapter = DogAdapter(dogImages)
+        binding.rvDogs.layoutManager=LinearLayoutManager(this)
+        binding.rvDogs.adapter = adapter
+    }
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        //todo lo que este en launch sera ejecutado en otro hilo
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService1::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            //todo lo que se ejecute en runOnUiThread va ejecutar en el hilo principal
+            runOnUiThread {
+                if (call.isSuccessful){
+                    val images = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    adapter.notifyDataSetChanged()
+                }
+                else{
+                    showError()
+                }
+                hideKeyBoard()
+            }
+
+        }
+    }
+
+    private fun hideKeyBoard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewRoot.windowToken,0)
+    }
+
+    private fun showError() {
+        Toast.makeText(this,"Ha ocurrido un error",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchByName(query.toLowerCase())
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 }
